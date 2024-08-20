0d6527b66979b5c40ab874f76b54433d50d7dcea
Marcus Vinicius <marcusvinicius2090@gmail.com>
2024-08-20 02:39:49 -0300

    Exemplo Pré Fixado de Private

---
+++ Programação Orientada a Objetos/PreFixadoExample.js
@@ -0,0 +1,29 @@
+class Pessoa {
+    constructor(nome, anoNascimento, profissao) {
+        this.nome = nome;
+        this.anoNascimento = anoNascimento;
+        this.profissao = profissao;
+        this.calculaIdade = function () {
+            return new Date().getFullYear() - this.anoNascimento;
+        }
+    };
+    ola() {
+        return 'olá ' + this.nome;
+    };
+};
+
+class Estudante extends Pessoa {
+    #matricula; // está privada por identificador
+    constructor(nome, anoNascimento, profissao, matricula) {
+        super(nome, anoNascimento, profissao);
+        this.#matricula = matricula;
+        this.#ola();
+    };
+    #ola(){ //está privada por identificador
+        super.ola();
+        return 'olá do aluno';
+    }
+};
+
+const estudante1 = new Estudante('Jorge', 2000, 'Estudante', 15236)
+console.log(estudante1);
