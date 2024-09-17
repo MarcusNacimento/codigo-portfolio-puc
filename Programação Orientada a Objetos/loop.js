6bc491e6f74e1937353b3d312cd51bd0bb7e836d
Marcus Vinicius <marcusvinicius2090@gmail.com>
2024-09-17 20:53:03 -0300

    Diferença entre loop com in, of e valores definidos

---
+++ Programação Orientada a Objetos/loop.js
@@ -0,0 +1,27 @@
+var a = []
+a[0] = Math.random()
+a[1] = Math.random()
+a[9] = Math.random()
+
+console.log(a.length)
+console.log(" -for with in- ")
+console.log("-=========-")
+
+for (let val in a){
+    console.log("=>" + val)
+    console.log("-=-=-=-=-=-\n")
+}
+
+console.log(" -for with in- ")
+console.log("-=========-")
+
+for (let val of a){
+    console.log("=>" + val)
+    console.log("-=-=-=-=-=-\n")
+}
+
+console.log(" -for with (;;))- ")
+console.log("-=========-")
+for(let i=0;i<a.length;i++){
+    console.log(" -->"+i+": "+a[i] )
+}
