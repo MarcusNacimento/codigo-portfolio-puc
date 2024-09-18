f8994c905dc489643a7cb501f97d54d48324972a
Marcus Vinicius <marcusvinicius2090@gmail.com>
2024-09-17 21:23:53 -0300

    Função como parâmetro

---
+++ Programação Orientada a Objetos/FuncaoComoParametro.js
@@ -0,0 +1,16 @@
+function decision(question, doOK, doCancel){
+    if(question=="OK") doOK()
+        else 
+            doCancel();
+    }
+
+    function showOk(){
+        console.log("You agreed");
+    }
+
+    function showCancel(){
+        console.log("You canceled this execution");
+    }
+
+    decision("OK",showOk, showCancel);
+    decision("Cancel", showOk,showCancel);
